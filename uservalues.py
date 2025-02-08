ae1b7340abbfaf3534f116577d27082ba6d455cc
Urooj <uroojasad70904@gmail.com>
2025-02-07 19:46:22 -0800

    Add files via upload

---
+++ uservalues.py
@@ -0,0 +1,9 @@
+num1=5 
+num2=3
+num3= 7
+temp=0
+print("lets exchange the values of the variables")
+print("value of temp is:", num1)
+print("value of num1 is:", num2)
+print("value of num2 is :", num3)
+print("value of num3 is:", num1)
