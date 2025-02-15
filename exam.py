19b6a3ba3f0fc5fd3555c098be7f673435d35a43
Urooj <uroojasad70904@gmail.com>
2025-02-14 20:21:35 -0800

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your current age"))
+if age == 15:
+    print("YES! You can take the Examination")
+elif age > 15:
+    print("YOU can take the  Examination")
+elif age < 15:
+    print("You are NOT Eligibile for the Examination")    
+else:
+    print(" You can no take the Examination")
+    
