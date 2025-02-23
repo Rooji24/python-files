1ef26766ef0fbf7c6894436edbd1fe263e09cdb1
Urooj <uroojasad70904@gmail.com>
2025-02-23 07:45:16 -0800

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,8 @@
+setc1 = {"green", "blue"}
+setc2 = {"blue", "yellow"}
+print("orignial sets")
+print(setc1)
+print(setc2)
+setc = setc1.intersection(setc2)
+print("\n intersection of above sets:")
+print(setc)
